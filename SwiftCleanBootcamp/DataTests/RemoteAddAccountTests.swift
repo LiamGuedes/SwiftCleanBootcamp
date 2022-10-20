4db6c958c4b4b05bfc9f5cee662d779c388b50b9
Willian Guedes <willianguedes@Willians-MacBook-Pro.local>
2022-10-20 19:32:33 -0300

    style(feature/DomainLayer): refactor test and httpClient protocol

--- SwiftCleanBootcamp/DataTests/RemoteAddAccountTests.swift
+++ SwiftCleanBootcamp/DataTests/RemoteAddAccountTests.swift
@@ -8,15 +8,15 @@
 import XCTest
 @testable import Data
 
-protocol HttpClient {
+protocol HttpPostClient {
     func post( url: URL)
 }
 
 class RemoteAddAccount {
     private let url: URL
-    private let httpClient: HttpClient
+    private let httpClient: HttpPostClient
     
-    init(url: URL, httpClient: HttpClient) {
+    init(url: URL, httpClient: HttpPostClient) {
         self.url = url
         self.httpClient = httpClient
     }
@@ -27,7 +27,7 @@ class RemoteAddAccount {
 }
 
 final class RemoteAddAccountTests: XCTestCase {
-    func test_() {
+    func test_add_should_call_httpClient_with_correct_url() {
         let url = URL(string: "https://www.google.com.br")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
@@ -41,7 +41,7 @@ class HttpClientSpy {
     var url: URL?
 }
 
-extension HttpClientSpy: HttpClient {
+extension HttpClientSpy: HttpPostClient {
     func post(url: URL) {
         self.url = url
     }
