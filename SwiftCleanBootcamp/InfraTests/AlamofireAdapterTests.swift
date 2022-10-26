87569e1f677b02ff14ba584badbf6ee325cfe4b0
LiamGuedes <liam.guedes.cunha@gmail.com>
2022-10-26 13:15:17 -0300

    test(feature/InfraLayer): Add nil body test case

--- SwiftCleanBootcamp/InfraTests/AlamofireAdapterTests.swift
+++ SwiftCleanBootcamp/InfraTests/AlamofireAdapterTests.swift
@@ -18,7 +18,7 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        let json = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
@@ -40,6 +40,28 @@ class AlamofireAdapterTests: XCTestCase {
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_post_should_make_request_with_no_data() {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+
+        let url = makeURL()
+        
+        sut.post(to: url, with: nil)
+        
+        let exp = expectation(description: "waiting")
+        URLProtocolStub.requestObserver { request in
+            XCTAssertEqual(url, request.url)
+            XCTAssertEqual("POST", request.httpMethod)
+            XCTAssertNil(request.httpBodyStream)
             exp.fulfill()
         }
         
